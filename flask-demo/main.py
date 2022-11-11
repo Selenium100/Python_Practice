7b588c49f694627ab148d7489a3ad7427d046c0e
Nitya <nityaranjan190592@gmail.com>
2022-11-11 22:56:56 +0530

    slug to fetch blog from database code pushed

--- flask-demo/main.py
+++ flask-demo/main.py
@@ -10,11 +10,11 @@ with open("config.json", "r") as c:
 db = SQLAlchemy()
 app = Flask(__name__)
 app.config.update(
-    MAIL_SERVER = 'smtp.gmail.com',
-    MAIL_PORT = '465',
-    MAIL_USE_SSL = True,
-    MAIL_USERNAME = params['gmail-user'],
-    MAIL_PASSWORD = params['gmail-password']
+    MAIL_SERVER='smtp.gmail.com',
+    MAIL_PORT='465',
+    MAIL_USE_SSL=True,
+    MAIL_USERNAME=params['gmail-user'],
+    MAIL_PASSWORD=params['gmail-password']
 )
 mail = Mail(app)
 if params['local_server']:
@@ -30,9 +30,24 @@ class Mydata(db.Model):
     subject = db.Column(db.String(20), nullable=False)
 
 
+class Blog(db.Model):
+    sno = db.Column(db.Integer, primary_key=True)
+    slug = db.Column(db.String(80), nullable=False)
+    heading = db.Column(db.String(80), nullable=False)
+    subheading = db.Column(db.String(20), nullable=False)
+    date = db.Column(db.String(20), nullable=False)
+    content = db.Column(db.String(10000), nullable=False)
+
+
 @app.route('/')
 def welcome():
     return "Welcome to Flask app"
+
+
+@app.route('/blog/<string:blog_slug>', methods=['GET'])
+def blog(blog_slug):
+    blog = Blog.query.filter_by(slug=blog_slug).first()
+    return render_template("blog.html", blog=blog)
 
 
 @app.route('/contact', methods=['GET', 'POST'])
@@ -42,10 +57,11 @@ def contact():
         fname1 = request.form.get("fname")
         lname1 = request.form.get("lname")
         fsubject1 = request.form.get("fsubject")
-        entry = Mydata(firstname=fname1, lastname=lname1 , subject=fsubject1)
+        entry = Mydata(firstname=fname1, lastname=lname1, subject=fsubject1)
         db.session.add(entry)
         db.session.commit()
-        mail.send_message(f'New message by {fname1}',sender = 'nityaranjan190592@gmail.com', recipients = [params['gmail-user']],body = fsubject1)
+        mail.send_message(f'New message by {fname1}', sender='nityaranjan190592@gmail.com',
+                          recipients=[params['gmail-user']], body=fsubject1)
 
     return render_template("index.html", params=params)
 
